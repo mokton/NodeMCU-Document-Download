928b5b11e74b77abbb6d1cdba6811b4e5ddddd1e
Angus Meng <mokton@users.noreply.github.com>
2017-01-10 14:50:01 +0800

    Automatically change url links in the html files.
    Automatically change url links in the html files.

--- download.py
+++ download.py
@@ -6,7 +6,7 @@ Created on Tue Jan 10 09:10:48 2017
 """
 
 import urllib2
-import os
+import os, re
 
 modules = ["adc","adxl345","am2320","apa102","bit","bme280",
             "bmp085","cjson","coap","crypto","dht","encoder","enduser-setup",
@@ -14,7 +14,7 @@ modules = ["adc","adxl345","am2320","apa102","bit","bme280",
             "mqtt","net","node","ow","pcm","perf","pwm","rc","rotary",
             "rtcfifo","rtcmem","rtctime","sigma-delta","sntp","somfy","spi",
             "struct","switec","tm1829","tmr","tsl2561","u8g","uart","ucg",
-            "wifi","ws2801","ws2812","cron"]
+            "wifi","ws2801","ws2812","websocket","cron"]
 
 css = ["http://nodemcu.readthedocs.io/en/master/css/theme.css",
        "http://nodemcu.readthedocs.io/en/master/css/theme_extra.css",
@@ -31,6 +31,8 @@ js = ["http://nodemcu.readthedocs.io/en/master/js/jquery-2.1.1.min.js",
       "https://media.readthedocs.org/static/core/js/readthedocs-doc-embed.js",
       "http://nodemcu.readthedocs.io/en/master/readthedocs-data.js",
       "http://nodemcu.readthedocs.io/en/master/readthedocs-dynamic-include.js"]
+
+icon = "http://nodemcu.readthedocs.io/en/master/img/favicon.png"
 
 def get_page(url):  
     
@@ -52,7 +54,7 @@ def download(level = "dev", single = False, module = ""):
     home = "http://nodemcu.readthedocs.io/en/" + level + "/en/modules/"
     direct_path = os.path.abspath(os.curdir) + "/" + level
     if not os.path.exists(direct_path):
-        os.mkdir(direct_path) #在本地新建文件夹
+        os.mkdir(direct_path)
     if single:
         try:
             url = home + module
@@ -87,14 +89,28 @@ def replace_content(level = "dev", single = False, module = ""):
     direct_path = os.path.abspath(os.curdir) + "/" + level
     if not os.path.exists(direct_path):
         os.mkdir(direct_path) 
+    p1 = re.compile(r'<li class="toctree-l1 ">(.*)<li><span>Modules</span></li>', re.S)
+    p2 = re.compile(r'<li class="toctree-l1 ">(.*?)<a class="" href="(.*?)">(.*?)</a>(.*?)</li>', re.S)
+    p3 = re.compile(r'<div class="rst-versions".*</div>', re.S)
     if single:
         try:
             filename = direct_path + "/" + module + ".html"
             f = open(filename, "r")
             content = str(f.read())
             f.close()
+            #print(p2.findall(content))
+            content = p1.sub('<li class="toctree-l1 "><span>Modules</span></li>', content, 1)
+            content = p2.sub('<li class="toctree-l1 ">\g<1><a class="" href="\g<3>.html">\g<3></a>\g<4></li>', content)
+            content = p3.sub('', content)
+            content = content.replace("../../../img/favicon", "favicon")
             content = content.replace("../../../css/", "css/")
             content = content.replace("https://media.readthedocs.org//css/", "css/")
+            content = content.replace("../../../js/", "js/")
+            content = content.replace("https://media.readthedocs.org/static/core/js/", "js/")
+            content = content.replace("../../../readthedocs", "js/readthedocs")
+            content = content.replace("enduser setup.html", "enduser-setup.html")
+            content = content.replace("ow (1-Wire).html", "ow.html")
+            content = content.replace("sigma delta.html", "sigma-delta.html")
             #print(content)
             f = open(filename, "w")
             f.write(content)
@@ -109,11 +125,18 @@ def replace_content(level = "dev", single = False, module = ""):
                 f = open(filename, "r")
                 content = str(f.read())
                 f.close()
+                content = p1.sub('<li class="toctree-l1 "><span>Modules</span></li>', content, 1)
+                content = p2.sub('<li class="toctree-l1 ">\g<1><a class="" href="\g<3>.html">\g<3></a>\g<4></li>', content)
+                content = p3.sub('', content)
+                content = content.replace("../../../img/favicon", "favicon")
                 content = content.replace("../../../css/", "css/")
                 content = content.replace("https://media.readthedocs.org//css/", "css/")
                 content = content.replace("../../../js/", "js/")
                 content = content.replace("https://media.readthedocs.org/static/core/js/", "js/")
                 content = content.replace("../../../readthedocs", "js/readthedocs")
+                content = content.replace("enduser setup.html", "enduser-setup.html")
+                content = content.replace("ow (1-Wire).html", "ow.html")
+                content = content.replace("sigma delta.html", "sigma-delta.html")
                 #print(content)
                 f = open(filename, "w")
                 f.write(content)
@@ -157,6 +180,19 @@ def css_js(level = "dev"):
                 print(fn + " is empty.")
         except Exception, e:
             print e
+    try:
+        html = get_page(icon)
+        fn = os.path.basename(url)
+        if html <> "":
+            filename = direct_path + "/" + fn
+            f = open(filename, "w")
+            f.write(html)
+            f.close()
+            print(fn + " download is done.")
+        else:
+            print(fn + " is empty.")
+    except Exception, e:
+        print e
 
 if __name__ == "__main__":
     #modules.sort()
@@ -168,4 +204,8 @@ if __name__ == "__main__":
     #download(level=level, single=True,module="cron")
     replace_content(level=level)
     #replace_content(level=level, single=True,module="cron")
-    css_js(level=level)
+    #css_js(level=level)
+    
+    #download(level=level, single=True,module="websocket")
+    #replace_content(level=level, single=True,module="websocket")
+    
